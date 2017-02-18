57e0862c601ba51513348235f1aee901e76e1888
yance <yance@email.com>
2017-02-19 02:45:34 +0800

    d

--- cgi/login.php
+++ cgi/login.php
@@ -1,5 +1,4 @@
 <?php
 include '../.include.php';
 echo(get("t"));
-echo(Fun\get("t"));
 print_r($_GET);
--- db/db.php
+++ db/db.php
@@ -8,7 +8,7 @@ date_default_timezone_set('PRC');
 
 //Using Mysqli
 $dbc = new mysqli(DB_HOST,DB_USER,DB_PWD,DB_DBNAME);
-var_dump($dbc);
+if(!$dbc->client_info)die('db error 8');
 $db_char = DB_CHARSET;
 $dbc->query("SET NAMES utf8");
 $dbc->query("SET time_zone = '+8:00'");
